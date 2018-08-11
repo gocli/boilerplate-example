e01af32b2c4e4093b905ed6dff31b09befeceb8a
Stanislav Termosa <termosa.stanislav@gmail.com>
2018-08-11 15:50:03 +0300

    add install command

--- gofile.js
+++ gofile.js
@@ -1,22 +1,29 @@
 const go = require('go')
+const opn = require('opn')
 const setCmd = require('./commands/set')
 const clearCmd = require('./commands/clear')
 const translationsCmd = require('./commands/translations')
 const generate = require('./commands/generate')
 
-go.registerCommand('ping', () => console.log('pong')) // cli
+go.registerCommand('ping', () => console.log('pong'))
 
 go.registerCommand('pong', async () => {
   if (await go.confirm('did you mean ping?')) {
     console.log("> ping")
     go.executeCommand('ping')
   }
-}) // cli, quiz
+})
 
-go.registerCommand(setCmd) // cli, fs, quiz
+go.registerCommand('install', async () => {
+  console.log('Welcome to Go Boilerplate Example!')
+  if (await go.confirm('Do you want to read more about it?')) opn('http://gocli.io', { wait: false })
+  console.log('To try out this template navigate to its folder and run:\n $ go')
+})
 
-go.registerCommand(clearCmd) // cli, quiz
+go.registerCommand(setCmd)
 
-go.registerCommand(translationsCmd) // cli, fs, quiz
+go.registerCommand(clearCmd)
 
-go.registerCommand(generate) // cli, template, quiz
+go.registerCommand(translationsCmd)
+
+go.registerCommand(generate)
